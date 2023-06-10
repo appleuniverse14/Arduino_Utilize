e0f5a622d26fe94950eeb23b43374296ea063284
taizo122 <pocketspla@gmail.com>
2023-06-10 15:36:52 +0900

    add: serCom.py

--- LEDtape/get_weather/main.py
+++ LEDtape/get_weather/main.py
@@ -1,1 +1,5 @@
+import scrape
+
+print(scrape.getChanceOfRain())
+
 print("Hello, world!")
--- LEDtape/get_weather/serCom.py
+++ LEDtape/get_weather/serCom.py
@@ -1,2 +1,19 @@
-print("Hello, world!")
+import serial
 
+
+def serialCommunication(rain):
+    with serial.Serial("", 9600, timeout=1) as ser:
+        if rain:
+            val = "1"  # rain
+        else:
+            val = "0"  # not rain
+        data = bytes(val, "utf-8")
+        ser.write(data)
+
+        ser.close()
+
+
+if __name__ == "__main__":
+    serialCommunication()
+
+print("Hello, world!")
